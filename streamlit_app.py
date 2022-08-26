d09f9136c1fb5b7db3177c8a0149528c3465d69e
ofirAbu <ofir.dev@gmail.com>
2022-08-26 17:44:52 +0300

    fix: prettified st app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,9 @@ st.title("Semgrep Analyzer")
 language = st.selectbox("Select Language to Analyze", list(languages.keys()))
 
 vulnerability_hist_owasp = get_plotly_histogram(language_path=languages[language],
-                                          vulnerability_classifier='owasp')
+                                                vulnerability_classifier='owasp')
 vulnerability_hist_cwe = get_plotly_histogram(language_path=languages[language],
-                                          vulnerability_classifier='cwe')
+                                              vulnerability_classifier='cwe')
 
 st.plotly_chart(vulnerability_hist_owasp)
 st.plotly_chart(vulnerability_hist_cwe)
